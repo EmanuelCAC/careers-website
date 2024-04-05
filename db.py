bf77bf355fc9dd505fd6afc8042de363adbb44ab
EmanuelCAC <emanuelc279@gmail.com>
2024-04-05 14:26:49 -0300

    dynamic pages for each job

--- db.py
+++ db.py
@@ -11,3 +11,14 @@ def load_jobs_from_db():
     jobs = [dict(row) for row in res.fetchall()]
     con.close()
     return jobs
+
+def load_job_from_db(id):
+    con = sqlite3.connect(db_connection)
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM jobs Where id = ?", id)
+    job = [dict(row) for row in res.fetchall()]
+    if job == []:
+        return None
+    con.close()
+    return job[0]
